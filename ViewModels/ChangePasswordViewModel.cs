d83c84ecff53a792b5e0f579f293c6e860ce25de
balu <168086202+baluputta@users.noreply.github.com>
2024-10-19 14:42:37 +0530

    Add files via upload

---
+++ ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "ReEnter.")]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match.")]
+        [Display(Name = "New Password.")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "ConfirmNewPassword is require.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "ReEnter.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "ConfirmPassword is require.")]
+        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
